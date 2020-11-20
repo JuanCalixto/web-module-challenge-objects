a4e573d363940cf0acbf4fbcad66db1ce2226ec2
JuanCalixto <juancalixtomorales@gmail.com>
2020-11-19 22:16:44 -0600

    getReviewByRating stretch task 1 complete

--- index.js
+++ index.js
@@ -167,10 +167,18 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
-  }
+function getReviewByRating(array, rating) {
+  let newArray = [];
+  
+   for (let i = 0; i < array.length; i++) {
+     if(array[i].rating <= rating) {
+      newArray.push(array[i]);
+   }
+}
+  return newArray;
+ }
 
+ getReviewByRating(reviews, 4) 
   
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪💪   
 Use the getLongReviews function below to do the following:
